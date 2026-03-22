0da2e729d38d7511c7b98c4f1f5e0afc783ea501
Maquiz1 <manquiz92@gmail.com>
2026-03-22 23:49:34 +0300

    fix

--- backend/choices/models/base_model/base_choice_model.py
+++ backend/choices/models/base_model/base_choice_model.py
@@ -12,7 +12,7 @@ class BaseChoiceModel(AuditModel):
 
     class Meta:
         abstract = True
-        ordering = ["id"]
+        ordering = ["id","code"]
 
     def __str__(self):
         return f"{self.code} - ({self.name})"
