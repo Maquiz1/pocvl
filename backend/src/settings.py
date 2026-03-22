266028f88c8b3a61729572f154af00e75e7aa37e
Maquiz1 <manquiz92@gmail.com>
2026-03-22 13:28:51 +0300

    fix

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -144,8 +144,13 @@ USE_TZ = True
 
 TEMPLATES[0]['DIRS'] = [BASE_DIR / "templates"]
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
