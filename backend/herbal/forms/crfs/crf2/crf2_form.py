ed041826a81c9f9ce036c5d78b2bc7b644e5dbf8
Maquiz1 <manquiz92@gmail.com>
2026-03-25 07:41:19 +0300

    fix

--- backend/herbal/forms/crfs/crf2/crf2_form.py
+++ backend/herbal/forms/crfs/crf2/crf2_form.py
@@ -143,136 +143,227 @@ class CRF2Form(forms.ModelForm):
         # APPEARANCE
         # --------------------
         appearance = cleaned_data.get("appearance")
+        appearance_comments = cleaned_data.get("appearance_comments")
+        appearance_signif = cleaned_data.get("appearance_signifcnt")
 
         if appearance and appearance.id == 2:
-            if not cleaned_data.get("appearance_comments"):
-                self.add_error("appearance_comments", "This field is required")
-
-            if not cleaned_data.get("appearance_signifcnt"):
+            if not appearance_signif:
                 self.add_error("appearance_signifcnt", "This field is required")
-        else:
-            cleaned_data["appearance_comments"] = None
+        elif appearance:
+            if appearance_comments:
+                self.add_error("appearance_comments", "Must be empty when not abnormal")
+            if appearance_signif:
+                self.add_error("appearance_signifcnt", "Must be empty when not abnormal")
+            cleaned_data["appearance_comments"] = ""
             cleaned_data["appearance_signifcnt"] = None
+
 
         # --------------------
         # HEENT
         # --------------------
         heent = cleaned_data.get("heent")
+        heent_comments = cleaned_data.get("heent_comments")
+        heent_signif = cleaned_data.get("heent_signifcnt")
 
         if heent and heent.id == 2:
-            if not cleaned_data.get("heent_signifcnt"):
+            if not heent_signif:
                 self.add_error("heent_signifcnt", "This field is required")
-        else:
+        elif heent:
+            if heent_comments:
+                self.add_error("heent_comments", "Must be empty when not abnormal")
+            if heent_signif:
+                self.add_error("heent_signifcnt", "Must be empty when not abnormal")
+            cleaned_data["heent_comments"] = ""
             cleaned_data["heent_signifcnt"] = None
+
 
         # --------------------
         # RESPIRATORY
         # --------------------
         respiratory = cleaned_data.get("respiratory")
+        respiratory_comments = cleaned_data.get("respiratory_comments")
+        respiratory_signif = cleaned_data.get("respiratory_signifcnt")
 
         if respiratory and respiratory.id == 2:
-            if not cleaned_data.get("respiratory_signifcnt"):
+            if not respiratory_signif:
                 self.add_error("respiratory_signifcnt", "This field is required")
-        else:
+        elif respiratory:
+            if respiratory_comments:
+                self.add_error("respiratory_comments", "Must be empty when not abnormal")
+            if respiratory_signif:
+                self.add_error("respiratory_signifcnt", "Must be empty when not abnormal")
+            cleaned_data["respiratory_comments"] = ""
             cleaned_data["respiratory_signifcnt"] = None
+
 
         # --------------------
         # CARDIOVASCULAR
         # --------------------
         cardiovascular = cleaned_data.get("cardiovascular")
+        cardiovascular_comments = cleaned_data.get("cardiovascular_comments")
+        cardiovascular_signif = cleaned_data.get("cardiovascular_signifcnt")
 
         if cardiovascular and cardiovascular.id == 2:
-            if not cleaned_data.get("cardiovascular_signifcnt"):
+            if not cardiovascular_signif:
                 self.add_error("cardiovascular_signifcnt", "This field is required")
-        else:
+        elif cardiovascular:
+            if cardiovascular_comments:
+                self.add_error("cardiovascular_comments", "Must be empty when not abnormal")
+            if cardiovascular_signif:
+                self.add_error("cardiovascular_signifcnt", "Must be empty when not abnormal")
+            cleaned_data["cardiovascular_comments"] = ""
             cleaned_data["cardiovascular_signifcnt"] = None
+
 
         # --------------------
         # ABDOMINAL
         # --------------------
         abdominal = cleaned_data.get("abdominal")
+        abdominal_comments = cleaned_data.get("abdominal_comments")
+        abdominal_signif = cleaned_data.get("abdominal_signifcnt")
 
         if abdominal and abdominal.id == 2:
-            if not cleaned_data.get("abdominal_signifcnt"):
+            if not abdominal_signif:
                 self.add_error("abdominal_signifcnt", "This field is required")
-        else:
+        elif abdominal:
+            if abdominal_comments:
+                self.add_error("abdominal_comments", "Must be empty when not abnormal")
+            if abdominal_signif:
+                self.add_error("abdominal_signifcnt", "Must be empty when not abnormal")
+            cleaned_data["abdominal_comments"] = ""
             cleaned_data["abdominal_signifcnt"] = None
+
 
         # --------------------
         # UROGENITAL
         # --------------------
         urogenital = cleaned_data.get("urogenital")
+        urogenital_comments = cleaned_data.get("urogenital_comments")
+        urogenital_signif = cleaned_data.get("urogenital_signifcnt")
 
         if urogenital and urogenital.id == 2:
-            if not cleaned_data.get("urogenital_signifcnt"):
+            if not urogenital_signif:
                 self.add_error("urogenital_signifcnt", "This field is required")
-        else:
+        elif urogenital:
+            if urogenital_comments:
+                self.add_error("urogenital_comments", "Must be empty when not abnormal")
+            if urogenital_signif:
+                self.add_error("urogenital_signifcnt", "Must be empty when not abnormal")
+            cleaned_data["urogenital_comments"] = ""
             cleaned_data["urogenital_signifcnt"] = None
+
 
         # --------------------
         # MUSCULOSKELETAL
         # --------------------
         musculoskeletal = cleaned_data.get("musculoskeletal")
+        musculoskeletal_comments = cleaned_data.get("musculoskeletal_comments")
+        musculoskeletal_signif = cleaned_data.get("musculoskeletal_signifcnt")
 
         if musculoskeletal and musculoskeletal.id == 2:
-            if not cleaned_data.get("musculoskeletal_signifcnt"):
+            if not musculoskeletal_signif:
                 self.add_error("musculoskeletal_signifcnt", "This field is required")
-        else:
+        elif musculoskeletal:
+            if musculoskeletal_comments:
+                self.add_error("musculoskeletal_comments", "Must be empty when not abnormal")
+            if musculoskeletal_signif:
+                self.add_error("musculoskeletal_signifcnt", "Must be empty when not abnormal")
+            cleaned_data["musculoskeletal_comments"] = ""
             cleaned_data["musculoskeletal_signifcnt"] = None
+
 
         # --------------------
         # NEUROLOGICAL
         # --------------------
         neurological = cleaned_data.get("neurological")
+        neurological_comments = cleaned_data.get("neurological_comments")
+        neurological_signif = cleaned_data.get("neurological_signifcnt")
 
         if neurological and neurological.id == 2:
-            if not cleaned_data.get("neurological_signifcnt"):
+            if not neurological_signif:
                 self.add_error("neurological_signifcnt", "This field is required")
-        else:
+        elif neurological:
+            if neurological_comments:
+                self.add_error("neurological_comments", "Must be empty when not abnormal")
+            if neurological_signif:
+                self.add_error("neurological_signifcnt", "Must be empty when not abnormal")
+            cleaned_data["neurological_comments"] = ""
             cleaned_data["neurological_signifcnt"] = None
+
 
         # --------------------
         # PSYCHOLOGICAL
         # --------------------
         psychological = cleaned_data.get("psychological")
+        psychological_comments = cleaned_data.get("psychological_comments")
+        psychological_signif = cleaned_data.get("psychological_signifcnt")
 
         if psychological and psychological.id == 2:
-            if not cleaned_data.get("psychological_signifcnt"):
+            if not psychological_signif:
                 self.add_error("psychological_signifcnt", "This field is required")
-        else:
+        elif psychological:
+            if psychological_comments:
+                self.add_error("psychological_comments", "Must be empty when not abnormal")
+            if psychological_signif:
+                self.add_error("psychological_signifcnt", "Must be empty when not abnormal")
+            cleaned_data["psychological_comments"] = ""
             cleaned_data["psychological_signifcnt"] = None
+
 
         # --------------------
         # ENDOCRINE
         # --------------------
         endocrine = cleaned_data.get("endocrine")
+        endocrine_comments = cleaned_data.get("endocrine_comments")
+        endocrine_signif = cleaned_data.get("endocrine_signifcnt")
 
         if endocrine and endocrine.id == 2:
-            if not cleaned_data.get("endocrine_signifcnt"):
+            if not endocrine_signif:
                 self.add_error("endocrine_signifcnt", "This field is required")
-        else:
+        elif endocrine:
+            if endocrine_comments:
+                self.add_error("endocrine_comments", "Must be empty when not abnormal")
+            if endocrine_signif:
+                self.add_error("endocrine_signifcnt", "Must be empty when not abnormal")
+            cleaned_data["endocrine_comments"] = ""
             cleaned_data["endocrine_signifcnt"] = None
+
 
         # --------------------
         # LYMPHATIC
         # --------------------
         lymphatic = cleaned_data.get("lymphatic")
+        lymphatic_comments = cleaned_data.get("lymphatic_comments")
+        lymphatic_signif = cleaned_data.get("lymphatic_signifcnt")
 
         if lymphatic and lymphatic.id == 2:
-            if not cleaned_data.get("lymphatic_signifcnt"):
+            if not lymphatic_signif:
                 self.add_error("lymphatic_signifcnt", "This field is required")
-        else:
+        elif lymphatic:
+            if lymphatic_comments:
+                self.add_error("lymphatic_comments", "Must be empty when not abnormal")
+            if lymphatic_signif:
+                self.add_error("lymphatic_signifcnt", "Must be empty when not abnormal")
+            cleaned_data["lymphatic_comments"] = ""
             cleaned_data["lymphatic_signifcnt"] = None
+
 
         # --------------------
         # SKIN
         # --------------------
         skin = cleaned_data.get("skin")
+        skin_comments = cleaned_data.get("skin_comments")
+        skin_signif = cleaned_data.get("skin_signifcnt")
 
         if skin and skin.id == 2:
-            if not cleaned_data.get("skin_signifcnt"):
+            if not skin_signif:
                 self.add_error("skin_signifcnt", "This field is required")
-        else:
+        elif skin:
+            if skin_comments:
+                self.add_error("skin_comments", "Must be empty when not abnormal")
+            if skin_signif:
+                self.add_error("skin_signifcnt", "Must be empty when not abnormal")
+            cleaned_data["skin_comments"] = ""
             cleaned_data["skin_signifcnt"] = None
             
             
